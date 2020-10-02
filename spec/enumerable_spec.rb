642eb697bc87b01b55cad22eb307f0b7a6688245
dasileker <dasileker@gmail.com>
2020-10-02 08:59:00 +0100

    added test for my_all & my_any

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -39,6 +39,30 @@ describe Enumerable do
 
   describe '#my_all?' do 
     it 'Returns true if all items have a lenght bigger than 3' do
-      expect(test_array_string.my_all? { |word| words.lenght })
+      expect(test_array_string.my_all? { |word| word.length >= 3}).to eq(true)
     end
+    it 'Returns false if all numbers in an array are smaller than 2' do
+      expect(test_array_numbers.my_all? { |i| i > 2 }).to eql(false)
+    end
+    it 'Return false if all numbers of range are smaller than 5' do
+      expect(test_range.my_all? { |i| i < 5 }).to eq(true)
+    end
+    it 'Return false if one of the items of the array is false or nill' do
+      expect(test_array_bool.my_all?).to eq(false)
+    end
+  end
+
+  describe '#my_any?' do
+    it 'Return true if one of the items of the array is true' do
+      expect(test_array_bool.my_any?).to eq(true)
+    end
+    it 'Returns false if no value is given' do
+      expect([].my_any?).to eq(false)
+    end
+    it 'Return true if any item of the array is equal to argument' do
+      expect(test_array_numbers.my_any?(11)).to eq(true)
+    end
+    
+  end
+
 end
